ca7b6746a0fbd8e94be4f87cdb32f642f612fc29
Peter Hajdu <peter.hajdu@balabit.com>
2015-11-13 15:36:23 +0100

    it picks the first commit on the changed file

--- gitmagic/fixupdestinationpicker.py
+++ gitmagic/fixupdestinationpicker.py
@@ -1,4 +1,13 @@
 class FixupDestinationPicker(object):
     def __init__(self, repo, commit_range):
-        pass
+        self._repo = repo
+        self._commit_range = commit_range
+
+    def pick(self, change):
+        return self._pick_commit_for_change(change)
+
+
+    def _pick_commit_for_change(self, change):
+        gen = self._repo.commit.iter_items(self._repo, self._repo.head, paths=change.file_name)
+        return next(gen)
 
--- gitmagic/tests/test_fixup_destination_picker.py
+++ gitmagic/tests/test_fixup_destination_picker.py
@@ -1,9 +1,17 @@
 import unittest
+from unittest import mock
 import gitmagic
 
 class TestFixupDestinationPicker(unittest.TestCase):
-    def test_that_it_is_instantiable(self):
-        repo=0
-        commit_range=0
-        gitmagic.FixupDestinationPicker(repo, commit_range)
+    def setUp(self):
+        self.changed_file_path = "a_file_path"
+        self.commit_object = self.changed_file_path
+        self.repo = mock.Mock()
+        self.repo.commit.iter_items.return_value = iter( [ self.commit_object ] )
+        self.commit_range = [self.commit_object, "something else"]
+        self.destination_picker = gitmagic.FixupDestinationPicker(self.repo, self.commit_range)
+
+    def test_that_it_picks_a_commit_on_the_changed_file(self):
+        commit = self.destination_picker.pick(gitmagic.Change(self.changed_file_path))
+        self.assertEquals(commit, self.commit_object)
 
