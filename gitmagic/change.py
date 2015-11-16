c628f51beed5ff73e777bd6f9522ddea820c7c6c
Zsigmond Adam Oliver <adam.zsigmond@balabit.com>
2015-11-16 21:08:04 +0100

    make Change class to be able to make diffs for git apply
    Signed-off-by: Zsigmond Adam Oliver <adam.zsigmond@balabit.com>

--- gitmagic/change.py
+++ gitmagic/change.py
@@ -1,19 +1,41 @@
 from difflib import SequenceMatcher
+from io import StringIO
+
+import git
 
 
 class Change(object):
-    def __init__(self, a_file_name: str, b_file_name: str, a_file_content: str,
-                 b_file_content: str, a_hunk: (int, int), b_hunk: (int, int), tag: str):
+    def __init__(self, a_file_name: str, b_file_name: str, a_file_content: [str],
+                 b_file_content: [str], a_hunk: (int, int), b_hunk: (int, int), tag: str):
         self.a_file_name = a_file_name
         self.b_file_name = b_file_name
         self.a_file_content = a_file_content
         self.b_file_content = b_file_content
+        self.a_hunk = a_hunk
+        self.b_hunk = b_hunk
+        self.tag = tag
+
+    def unified_diff(self) -> StringIO:
+        diff_stream = StringIO()
+        diff_stream.write('--- a/{}\n'.format(self.a_file_name))
+        diff_stream.write('+++ b/{}\n'.format(self.b_file_name))
+        diff_stream.write('@@ -{},{} +{},{} @@\n'.format(self.a_hunk[0], self.a_hunk[1], self.b_hunk[0], self.b_hunk[1]))
+        for line in self.a_file_content[max(self.a_hunk[0] - 3, 0):self.a_hunk[0]]:
+            diff_stream.write(' {}\n'.format(line))
+        for line in self.a_file_content[self.a_hunk[0]:self.a_hunk[1]]:
+            diff_stream.write('-{}\n'.format(line))
+        for line in self.b_file_content[self.b_hunk[0]:self.b_hunk[1]]:
+            diff_stream.write('+{}\n'.format(line))
+        for line in self.a_file_content[self.a_hunk[1]:self.a_hunk[1] + 3]:
+            diff_stream.write(' {}\n'.format(line))
+        diff_stream.seek(0)
+        return diff_stream
 
     def __set__(self, key, value):
         raise TypeError('Immutable class')
 
 
-def find_changes(repo):
+def find_changes(repo: git.repo.Repo) -> [Change]:
     while True:
         try:
             diff = repo.head.commit.diff(None)[0]
--- gitmagic/tests/test_change_finder.py
+++ gitmagic/tests/test_change_finder.py
@@ -1,22 +1,27 @@
 import unittest
 from unittest import mock
 
+import git
+
 import gitmagic
 
 
 class TestChangeFinder(unittest.TestCase):
     def setUp(self):
-        self._repo = mock.Mock()
+        self._repo = mock.Mock(spec=git.repo.Repo)
         self._diff_list = []
         self._diff_list_on_second_call = []
         self._repo.head.commit.diff.side_effect = [self._diff_list, self._diff_list_on_second_call, []]
-        self._diff_mock = mock.Mock(a_path="changed_file_name",
-                                    b_path="changed_file_name",
-                                    a_blob=mock.Mock(
-                                        data_stream=mock.Mock(read=mock.Mock(return_value=b'line1\nlinea\nline3'))),
-                                    b_blob=mock.Mock(
-                                        data_stream=mock.Mock(read=mock.Mock(return_value=b'line1\nlineb\nline3')))
-                                    )
+        self._diff_mock = mock.Mock(
+            a_path="changed_file_name",
+            b_path="changed_file_name",
+            a_blob=mock.Mock(
+                data_stream=mock.Mock(
+                    read=mock.Mock(return_value=b'line1\nlinea\nline3'))),
+            b_blob=mock.Mock(
+                data_stream=mock.Mock(
+                    read=mock.Mock(return_value=b'line1\nlineb\nline3')))
+        )
 
     def test_that_it_returns_the_list_of_changes(self):
         changes = gitmagic.find_changes(self._repo)
@@ -39,3 +44,46 @@ class TestChangeFinder(unittest.TestCase):
         self.assertEqual(self._repo.head.commit.diff.call_count, 1)
         next(changes_generator)
         self.assertEqual(self._repo.head.commit.diff.call_count, 2)
+
+
+class TestChange(unittest.TestCase):
+    def setUp(self):
+        orig = ['line0', 'line1', 'line2', 'line3',
+                'line4', 'line5', 'line6', 'line7']
+        new = ['newline'] + orig[:-1]
+        new[4] = 'replaced_line'
+        self.replace_unified_diff = gitmagic.Change(
+            'file1', 'file2', orig, new,
+            (3, 4), (4, 5), 'replace'
+        ).unified_diff().read()
+
+    def test_change_unified_diff_contains_filename(self):
+        self.assertIn('--- a/file1', self.replace_unified_diff)
+        self.assertIn('+++ b/file2', self.replace_unified_diff)
+
+    def test_change_unified_diff_contains_the_replace_diff(self):
+        self.assertIn('-line3', self.replace_unified_diff)
+        self.assertIn('+replaced_line', self.replace_unified_diff)
+
+    def test_diff_contains_the_context(self):
+        self.assertIn(' line0', self.replace_unified_diff)
+        self.assertIn(' line5', self.replace_unified_diff)
+
+    def test_diff_contains_the_line_numbers(self):
+        self.assertIn(
+            '@@ -{},{} +{},{} @@'.format(
+                3, 4,
+                4, 5),
+            self.replace_unified_diff
+        )
+
+    def test_diff_has_right_length(self):
+        lines = self.replace_unified_diff.rstrip('\n').split('\n')
+        header = 2
+        diffstat = 1
+        context = 6
+        deletions = 1
+        insertions = 1
+        self.assertEqual(len(lines),
+                         header + context + diffstat +
+                         deletions + insertions)
