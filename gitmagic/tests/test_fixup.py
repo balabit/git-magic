2f92e391a71a8a3967f1744692464360528716ee
Peter Hajdu <peter.hajdu@balabit.com>
2015-11-16 13:15:19 +0100

    it calls reset before the fixup procedure

--- gitmagic/tests/test_fixup.py
+++ gitmagic/tests/test_fixup.py
@@ -3,7 +3,12 @@ from unittest import mock
 from gitmagic import fixup
 
 class TestFixup(unittest.TestCase):
-    def test_that_it_can_be_called(self):
-        repo = mock.Mock()
-        fixup(repo);
+    def setUp(self):
+        repomock = mock.Mock()
+        repomock.reset.return_value = None
+        self.repo = repomock
+        fixup(self.repo);
+
+    def test_that_it_resets_the_index(self):
+        self.repo.reset.assert_called_with()
 
