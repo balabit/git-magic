5389ed887f942d43fc3f9285dada51a8ee564303
Peter Hajdu <peter.hajdu@balabit.com>
2015-11-16 14:12:49 +0100

    it picks destination commit for each change

--- gitmagic/fixup.py
+++ gitmagic/fixup.py
@@ -1,7 +1,7 @@
 import gitmagic
 
-def fixup(repo):
+def fixup(repo, destination_picker):
     repo.reset()
     for change in gitmagic.find_changes(repo):
-        pass
+        destination_picker.pick(change)
 
--- gitmagic/tests/test_fixup.py
+++ gitmagic/tests/test_fixup.py
@@ -2,20 +2,31 @@ import unittest
 from unittest import mock
 from gitmagic import fixup
 from gitmagic import find_changes
+from gitmagic import Change
 
 class TestFixup(unittest.TestCase):
 
-    @mock.patch('gitmagic.find_changes', return_value=iter([]))
-    def setUp(self, find_changes_mock):
+    @mock.patch('gitmagic.find_changes')
+    def setUp(self, find_changes ):
+        self.first_change = Change("dogfood")
+        find_changes.return_value = iter([self.first_change])
+        self.find_changes = find_changes
+
         repomock = mock.Mock()
         repomock.reset.return_value = None
         self.repo = repomock
-        self.find_changes_mock = find_changes_mock
-        fixup(self.repo);
+
+        destination_picker = mock.Mock()
+        self.destination_picker = destination_picker
+
+        self.destination_picker = destination_picker
+        fixup(self.repo, self.destination_picker);
 
     def test_that_it_resets_the_index(self):
         self.repo.reset.assert_called_with()
 
     def test_that_it_collects_changes(self):
-        self.find_changes_mock.assert_called_with(self.repo)
+        self.find_changes.assert_called_with(self.repo)
 
+    def test_that_it_picks_destination_commit_for_each_change_object(self):
+        self.destination_picker.pick.assert_called_with(self.first_change)
