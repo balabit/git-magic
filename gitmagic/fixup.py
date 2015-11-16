fc3a01057293317ade8b58713a646f2366a56619
Peter Hajdu <peter.hajdu@balabit.com>
2015-11-16 14:59:11 +0100

    add magic-fixup

--- gitmagic/fixup.py
+++ gitmagic/fixup.py
@@ -2,13 +2,13 @@ import gitmagic
 
 
 def fixup(repo, destination_picker):
-    repo.reset()
+    repo.index.reset()
     for change in gitmagic.find_changes(repo):
         _apply_change(repo, change)
         destination_commit = destination_picker.pick(change)
-        repo.index.commit( message = "fixup! {}".format(destination_commit.message()))
+        repo.index.commit( message = "fixup! {}".format(destination_commit.message))
 
 def _apply_change(repo, change):
     #todo: apply unified diff only
-    repo.index.add(change.file_name)
+    repo.index.add([change.file_name])
 
