a1d3acae1b8b5b16281e77cb6ea66eea34b14937
Peter Hajdu <peter.hajdu@balabit.com>
2015-11-16 13:39:31 +0100

    it collects changes

--- gitmagic/fixup.py
+++ gitmagic/fixup.py
@@ -1,3 +1,7 @@
+import gitmagic
+
 def fixup(repo):
     repo.reset()
+    for change in gitmagic.find_changes(repo):
+        pass
 
--- gitmagic/tests/test_fixup.py
+++ gitmagic/tests/test_fixup.py
@@ -1,14 +1,21 @@
 import unittest
 from unittest import mock
 from gitmagic import fixup
+from gitmagic import find_changes
 
 class TestFixup(unittest.TestCase):
-    def setUp(self):
+
+    @mock.patch('gitmagic.find_changes', return_value=iter([]))
+    def setUp(self, find_changes_mock):
         repomock = mock.Mock()
         repomock.reset.return_value = None
         self.repo = repomock
+        self.find_changes_mock = find_changes_mock
         fixup(self.repo);
 
     def test_that_it_resets_the_index(self):
         self.repo.reset.assert_called_with()
+
+    def test_that_it_collects_changes(self):
+        self.find_changes_mock.assert_called_with(self.repo)
 
