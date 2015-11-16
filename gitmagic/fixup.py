c7244b35550119a7354d91f2d5f2c2064360b441
Peter Hajdu <peter.hajdu@balabit.com>
2015-11-16 14:53:56 +0100

    apply whole file instead of a patch

--- gitmagic/fixup.py
+++ gitmagic/fixup.py
@@ -1,9 +1,14 @@
 import gitmagic
 
+
 def fixup(repo, destination_picker):
     repo.reset()
     for change in gitmagic.find_changes(repo):
+        _apply_change(repo, change)
         destination_commit = destination_picker.pick(change)
-        #todo: apply unified diff from change
         repo.index.commit( message = "fixup! {}".format(destination_commit.message()))
+
+def _apply_change(repo, change):
+    #todo: apply unified diff only
+    repo.index.add(change.file_name)
 
