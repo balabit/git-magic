41b679eab9f00e508ba0eea9f3e7ddef83238cca
Peter Hajdu <peter.hajdu@balabit.com>
2015-11-16 14:59:11 +0100

    add magic-fixup

--- gitmagic/tests/test_fixup.py
+++ gitmagic/tests/test_fixup.py
@@ -17,7 +17,7 @@ class TestFixup(unittest.TestCase):
         self.repo = repo
 
         self.destination_commit = mock.Mock()
-        self.destination_commit.message.return_value = "a commit message"
+        self.destination_commit.message = "a commit message"
         destination_picker = mock.Mock()
         destination_picker.pick.return_value = self.destination_commit
         self.destination_picker = destination_picker
@@ -26,7 +26,7 @@ class TestFixup(unittest.TestCase):
         fixup(self.repo, self.destination_picker);
 
     def test_that_it_resets_the_index(self):
-        self.repo.reset.assert_called_with()
+        self.repo.index.reset.assert_called_with()
 
     def test_that_it_collects_changes(self):
         self.find_changes.assert_called_with(self.repo)
@@ -35,6 +35,5 @@ class TestFixup(unittest.TestCase):
         self.destination_picker.pick.assert_called_with(self.first_change)
 
     def test_that_it_commits_the_change_with_the_proper_fixup_message(self):
-        self.destination_commit.message.assert_called_with()
-        self.repo.index.commit.assert_called_with(message = "fixup! {}".format(self.destination_commit.message.return_value))
+        self.repo.index.commit.assert_called_with(message = "fixup! {}".format(self.destination_commit.message))
 
