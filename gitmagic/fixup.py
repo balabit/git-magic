57c0a56dc8be69e77eca1bab457cfbff206faf35
Peter Hajdu <peter.hajdu@balabit.com>
2015-11-17 11:51:10 +0100

    use strategy pattern in fixup to get changes

--- gitmagic/fixup.py
+++ gitmagic/fixup.py
@@ -1,9 +1,9 @@
 import gitmagic
 
 
-def fixup(repo, destination_picker):
+def fixup(repo, destination_picker, change_finder):
     repo.index.reset()
-    for change in gitmagic.find_changes(repo):
+    for change in change_finder(repo):
         _apply_change(repo, change)
         destination_commit = destination_picker.pick(change)
         repo.index.commit( message = "fixup! {}".format(destination_commit.message))
--- gitmagic/tests/test_fixup.py
+++ gitmagic/tests/test_fixup.py
@@ -6,24 +6,17 @@ from gitmagic import Change
 
 class TestFixup(unittest.TestCase):
 
-    @mock.patch('gitmagic.find_changes')
-    def setUp(self, find_changes ):
-
-        self.first_change = mock.Mock()
-        find_changes.return_value = iter([self.first_change])
-        self.find_changes = find_changes
-
-        repo = mock.Mock()
-        self.repo = repo
-
+    def setUp(self):
+        self.repo = mock.Mock()
         self.destination_commit = mock.Mock()
         self.destination_commit.message = "a commit message"
-        destination_picker = mock.Mock()
-        destination_picker.pick.return_value = self.destination_commit
-        self.destination_picker = destination_picker
+        self.destination_picker = mock.Mock()
+        self.destination_picker.pick.return_value = self.destination_commit
 
-        self.destination_picker = destination_picker
-        fixup(self.repo, self.destination_picker);
+        self.first_change = mock.Mock()
+        self.find_changes = mock.Mock()
+        self.find_changes.return_value = iter([self.first_change])
+        fixup(self.repo, self.destination_picker, self.find_changes);
 
     def test_that_it_resets_the_index(self):
         self.repo.index.reset.assert_called_with()
