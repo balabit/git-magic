0352f542341fe25be74c0130e7e50394c6f0bb6d
Peter Hajdu <peter.hajdu@balabit.com>
2015-11-18 14:06:42 +0100

    add interactive message colorization

--- gitmagic/fixup.py
+++ gitmagic/fixup.py
@@ -12,11 +12,7 @@ def fixup(repo, destination_picker, change_finder, args={}):
             continue
 
         destination = destination_commits[0]
-        gitmagic.checkpoint("Should I create fixup commit for {} -> {}:{}\n{}".format(
-            change.a_file_name,
-            destination.hexsha[:7],
-            destination.summary,
-            change.diff), args)
+        gitmagic.checkpoint( _colorize_change(change, destination), args)
         repo.index.commit( message = "fixup! {}".format(destination.message))
 
 def _apply_change(repo, change):
@@ -28,3 +24,37 @@ def _apply_change(repo, change):
     git_ = git.cmd.Git(repo.working_dir)
     git_.execute(['git', 'apply', '--cache', file_name])
 
+NO_COLOR = "\033[0m"
+YELLOW = "\033[1;33m"
+WHITE = "\033[1;37m"
+GREEN = "\033[1;32m"
+RED = "\033[1;31m"
+BLUE = "\033[1;36m"
+
+def _colorize(message, color):
+    return "{}{}{}".format(color, message, NO_COLOR)
+
+def _colorize_change(change, commit):
+    message = _colorize("Should I create fixup commit for {} -> {}:{}\n".format(
+            change.a_file_name,
+            commit.hexsha[:7],
+            commit.summary), YELLOW)
+    message += _diff_colorizer(change.diff)
+    return message
+
+def _is_diff_header_line(line):
+    return line[:1] == "@" or line[:3] == "+++" or line[:3] == "---"
+
+def _diff_colorizer(diff):
+    colorized = ""
+    for line in diff.splitlines(keepends=True):
+        color = WHITE
+        first_char = line[:1]
+        if first_char == "-":
+            color = RED
+        if first_char == "+":
+            color = GREEN
+        if _is_diff_header_line(line):
+            color = BLUE
+        colorized += _colorize(line, color)
+    return colorized
