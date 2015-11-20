63278cbc7259a62afb7f4fd464abeb1eead0c111
Peter Hajdu <peter.hajdu@balabit.com>
2015-11-20 17:25:03 +0100

    add some information to the setup script

--- setup.py
+++ setup.py
@@ -5,6 +5,12 @@ setup(
         name = "gitmagic",
         version = "0.1",
         packages = find_packages(),
+        description = 'All the magical things that git misses.',
         scripts = ['git-magic-fixup'],
         install_requires = ['GitPython>=1.0.1'],
+        url = 'https://github.com/peterhajdu/gitmagic',
+        maintainer = 'Peter Hajdu',
+        maintainer_email = 'peter.ferenc.hajdu@gmail.com',
+        download_url = 'https://github.com/peterhajdu/gitmagic/tarball/0.1',
+        keywords = ['git', 'magic', 'fixup'],
         )
